bf8254d3507a1fc3adae33a2f1eba3ed7b89d8d8
Dan Brady <dannymb360@gmail.com>
2023-03-07 15:05:27 -0500

    new css

--- scripts/html.js
+++ scripts/html.js
@@ -11,6 +11,19 @@ export const CiderFalls = () => {
     </div>
     
     </div>
+    <div class ="services">
+    Services: <section id ="service--1">Rafting,</section>
+    <section id ="service--2">Canoeing,</section>
+    <section id ="service--3">Fishing,</section>
+    <section id ="service--4">Hiking,</section>
+    <section id ="service--5">Picnicking,</section>
+    <section id ="service--6">Rock Climbing,</section>
+    <section id ="service--7">Lodging,</section>
+    <section id ="service--8">Parking,</section>
+    <section id ="service--9">Information,</section>
+    <section id ="service--10">Zip Lines</section>
+    
+    </div>
     <div class ="main">
         <article class="allParks">
         <div class ="topParks">
@@ -42,9 +55,9 @@ export const CiderFalls = () => {
          </div>
          </div>
     <div class ="footer">
-    <article>Phone Number</article>
-    <article>Email</article>
-    <article>Street Address</article>
+    <article>Phone Number: (518) 824-6000</article>
+    <article>Email: CiderFalls@Gmail.com</article>
+    <article>Street Address: 89 Cider Falls Drive, Queensbury, NY 12804</article>
     
     </div>
     
