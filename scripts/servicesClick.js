1cbd555164db72bf59bf365dcc53981c3bf02290
Michael Stewart <michaelpstewart@comcast.net>
2023-03-07 14:11:37 -0600

    quick fixes to clickses

--- scripts/servicesClick.js
+++ scripts/servicesClick.js
@@ -4,37 +4,39 @@ const services = getServices()
 const sectionServices = getSectionServices()
 const sections = getSections()
 
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const itemClicked = clickEvent.target
-        if (itemClicked.id.startsWith("service")){
-            const [, servicePrimaryKey] = itemClicked.id.split("--")
+export const servicesClick = () => {
+    document.addEventListener(
+        "click",
+        (clickEvent) => {
+            const itemClicked = clickEvent.target
+            if (itemClicked.id.startsWith("service")) {
+                const [, servicePrimaryKey] = itemClicked.id.split("--")
 
-            let currentService = null
-            for (const service of services){
-                if (parseInt(servicePrimaryKey) === service.id){
-                    currentService = service
+                let currentService = null
+                for (const service of services) {
+                    if (parseInt(servicePrimaryKey) === service.id) {
+                        currentService = service
+                    }
                 }
-            }
 
-            let matchingSectionServices = []
-            for (const sectionService of sectionServices){
-                if (sectionService.serviceId === currentService.id){
-                    matchingSectionServices.push(sectionService)
+                let matchingSectionServices = []
+                for (const sectionService of sectionServices) {
+                    if (sectionService.serviceId === currentService.id) {
+                        matchingSectionServices.push(sectionService)
+                    }
                 }
-            }
 
-            let sectionList = []
-                for (const matchingSectionService of matchingSectionServices){
-                    for (const section of sections){
-                        if (matchingSectionService.sectionId === section.id){
+                let sectionList = []
+                for (const matchingSectionService of matchingSectionServices) {
+                    for (const section of sections) {
+                        if (matchingSectionService.sectionId === section.id) {
                             sectionList.push(`${section.name}`)
                         }
                     }
                 }
                 const joinedSectionList = sectionList.join("\n")
                 window.alert(`${joinedSectionList}`)
+            }
         }
-    }
-)
+    )
+}
