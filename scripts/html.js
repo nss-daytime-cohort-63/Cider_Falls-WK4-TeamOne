8a321046ea3a864c8245cc1bab3e5a17c39389d5
Dan Brady <dannymb360@gmail.com>
2023-03-07 12:05:40 -0500

    all css modules

---
+++ scripts/html.js
@@ -0,0 +1,50 @@
+export const CiderFalls = () => {
+    return `
+    <div class ="top">
+    <img src="https://freedesignfile.com/upload/2016/08/Green-park-logo-vectors-set-04.jpg">
+   
+    <div class ="header">
+        <article class ="head">Welcome to Cider Falls</article>
+    </div>
+    
+    </div>
+    <div class ="main">
+        <article class="allParks">
+        <div class ="topParks">
+            <section class="parks">
+                Park 1
+                </section>
+            <section class="parks">
+                Park 2
+                 </section>
+                <section class="parks">
+                Park 3
+                </section>
+            </div>
+            <div class ="bottomParks">
+                <section class="parks">
+                Park 4
+                </section>
+                <section class="parks">
+                Park 5
+                </section>
+                <section class="parks">
+                Park 5
+                </section>
+            </div>
+         </article>
+         <div class ="people">
+         list of people
+         </div>
+         </div>
+    <div class ="footer">
+    <article>Phone Number</article>
+    <article>Email</article>
+    <article>Street Address</article>
+    
+    </div>
+    
+
+    
+    `
+}
