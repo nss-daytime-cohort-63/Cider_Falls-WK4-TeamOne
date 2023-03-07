1a668a63a6c10d15e8b86105264c15a8020648ca
Conor Getty <conorgetty@gmail.com>
2023-03-07 13:49:43 -0600

    finished section click event

---
+++ scripts/servicesClick.js
@@ -0,0 +1,40 @@
+import { getServices, getSectionServices, getSections } from "./database";
+
+const services = getServices()
+const sectionServices = getSectionServices()
+const sections = getSections()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("service")){
+            const [, servicePrimaryKey] = itemClicked.id.split("--")
+
+            let currentService = null
+            for (const service of services){
+                if (parseInt(servicePrimaryKey) === service.id){
+                    currentService = service
+                }
+            }
+
+            let matchingSectionServices = []
+            for (const sectionService of sectionServices){
+                if (sectionService.serviceId === currentService.id){
+                    matchingSectionServices.push(sectionService)
+                }
+            }
+
+            let sectionList = []
+                for (const matchingSectionService of matchingSectionServices){
+                    for (const section of sections){
+                        if (matchingSectionService.sectionId === section.id){
+                            sectionList.push(`${section.name}`)
+                        }
+                    }
+                }
+                const joinedSectionList = sectionList.join("\n")
+                window.alert(`${joinedSectionList}`)
+        }
+    }
+)
